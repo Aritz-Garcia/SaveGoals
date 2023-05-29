a63eef0003fa2911ed494aee837a2d5ff4a78847
Aritz-Garcia <73273260+Aritz-Garcia@users.noreply.github.com>
2023-05-29 12:35:17 +0200

    Update ObjetivosFragment.java

--- SaveGoals/app/src/main/java/com/savegoals/savegoals/Controlador/menu/ObjetivosFragment.java
+++ SaveGoals/app/src/main/java/com/savegoals/savegoals/Controlador/menu/ObjetivosFragment.java
@@ -145,6 +145,88 @@ public class ObjetivosFragment extends Fragment {
 
         if (objetivosCompletados.size() != 0) {
             tv_cumplidos.setVisibility(View.VISIBLE);
+            for (int i = 0; i < objetivosCompletados.size(); i++) {
+                int porcentajeInt = (int) ((objetivosCompletados.get(i).getAhorrado() * 100) / objetivosCompletados.get(i).getCantidad());
+
+                LinearLayout linearLayoutGeneral = new LinearLayout(getContext());
+                LinearLayout.LayoutParams paramsGeneral = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                paramsGeneral.setMargins(30, 50, 30, 0);
+                linearLayoutGeneral.setLayoutParams(paramsGeneral);
+                linearLayoutGeneral.setOrientation(LinearLayout.VERTICAL);
+
+                LinearLayout linearLayoutMedio = new LinearLayout(getContext());
+                linearLayoutMedio.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                linearLayoutMedio.setOrientation(LinearLayout.HORIZONTAL);
+
+                LinearLayout linearLayoutPeq = new LinearLayout(getContext());
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                params.width = 0;
+                params.weight = 4;
+                linearLayoutPeq.setLayoutParams(params);
+                linearLayoutPeq.setOrientation(LinearLayout.VERTICAL);
+
+                LinearLayout linearLayoutProgressBar = new LinearLayout(getContext());
+                LinearLayout.LayoutParams paramsProgressBar = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                paramsProgressBar.setMargins(30, 20, 30, 0);
+                linearLayoutProgressBar.setLayoutParams(paramsProgressBar);
+                linearLayoutProgressBar.setOrientation(LinearLayout.HORIZONTAL);
+
+                TextView nombre = new TextView(getContext());
+                nombre.setText(objetivosCompletados.get(i).getNombre());
+                nombre.setTextSize(18);
+                LinearLayout.LayoutParams paramsNombre = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                paramsNombre.width = 0;
+                paramsNombre.weight = 6;
+                nombre.setLayoutParams(paramsNombre);
+                nombre.setGravity(Gravity.CENTER);
+
+                TextView icono = new TextView(getContext());
+                icono.setText("prueba");
+                icono.setTextSize(18);
+                LinearLayout.LayoutParams paramsIcono = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                paramsIcono.width = 0;
+                paramsIcono.weight = 1;
+                icono.setLayoutParams(paramsIcono);
+
+                TextView fecha = new TextView(getContext());
+                fecha.setText("Fecha: " + objetivosCompletados.get(i).getFecha());
+
+                TextView cantidad = new TextView(getContext());
+                cantidad.setText(obtieneDosDecimales(objetivosCompletados.get(i).getAhorrado()) + "€ / " + obtieneDosDecimales(objetivosCompletados.get(i).getCantidad()) + "€");
+
+                ProgressBar progressBar = new ProgressBar(getContext(), null, android.R.attr.progressBarStyleHorizontal);
+                LinearLayout.LayoutParams paramsProgress = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                paramsProgress.width = 0;
+                paramsProgress.weight = 9;
+                paramsProgress.setMargins(0, 0, 50, 0);
+                progressBar.setLayoutParams(paramsProgress);
+                progressBar.setMax(100);
+                progressBar.setProgress(porcentajeInt);
+                progressBar.setProgressDrawable(getResources().getDrawable(R.drawable.custom_progressbar_verde));
+
+                TextView porcentaje = new TextView(getContext());
+                porcentaje.setText(porcentajeInt + "%");
+                LinearLayout.LayoutParams paramsPorcentaje = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                paramsPorcentaje.width = 0;
+                paramsPorcentaje.weight = 1;
+                porcentaje.setLayoutParams(paramsPorcentaje);
+
+                // Add views
+                linearLayoutPeq.addView(cantidad);
+                linearLayoutPeq.addView(fecha);
+
+                linearLayoutMedio.addView(icono);
+                linearLayoutMedio.addView(nombre);
+                linearLayoutMedio.addView(linearLayoutPeq);
+
+                linearLayoutGeneral.addView(linearLayoutMedio);
+
+                linearLayoutProgressBar.addView(progressBar);
+                linearLayoutProgressBar.addView(porcentaje);
+
+                linearLayoutGeneral.addView(linearLayoutProgressBar);
+                linearLayoutObjetivos.addView(linearLayoutGeneral);
+            }
         } else {
             tv_cumplidos.setVisibility(View.GONE);
         }
@@ -157,4 +239,6 @@ public class ObjetivosFragment extends Fragment {
         DecimalFormat df = new DecimalFormat("0.00");
         return df.format(value);
     }
+
+    
 }
