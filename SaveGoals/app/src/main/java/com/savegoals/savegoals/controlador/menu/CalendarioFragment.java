c00057cc3ac4075f85fa64c7ccc3e64481939568
Aritz-Garcia <73273260+Aritz-Garcia@users.noreply.github.com>
2023-06-13 14:02:44 +0200

    calendario view

--- SaveGoals/app/src/main/java/com/savegoals/savegoals/controlador/menu/CalendarioFragment.java
+++ SaveGoals/app/src/main/java/com/savegoals/savegoals/controlador/menu/CalendarioFragment.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
 import com.savegoals.savegoals.R;
+import com.savegoals.savegoals.data.entities.Objetivos;
+import com.savegoals.savegoals.db.AppDatabase;
+
+import java.util.List;
 
 public class CalendarioFragment extends Fragment {
+
+    LinearLayout lyCalDatos;
 
     public CalendarioFragment() {
         // Required empty public constructor
@@ -24,7 +31,19 @@ public class CalendarioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendario, container, false);
-        // Inflate the layout for this fragment
+
+        lyCalDatos = view.findViewById(R.id.lyCalDatos);
+
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        AppDatabase db = AppDatabase.getDatabase(getContext());
+
+        List<Objetivos> objetivos = db.objetivosDao().getAll();
+
     }
 }
