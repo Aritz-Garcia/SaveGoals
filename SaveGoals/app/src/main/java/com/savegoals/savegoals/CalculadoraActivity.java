76a10a80685e6deaad3b53d1ae6cd3c7b3f95a87
Aritz-Garcia <73273260+Aritz-Garcia@users.noreply.github.com>
2023-12-23 13:50:51 +0100

    Calculadora con precio inicial

--- SaveGoals/app/src/main/java/com/savegoals/savegoals/CalculadoraActivity.java
+++ SaveGoals/app/src/main/java/com/savegoals/savegoals/CalculadoraActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,9 +17,10 @@ import java.time.format.DateTimeFormatter;
 public class CalculadoraActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView tvResultado, tvTextCrearCalc;
-    EditText etCantidad, etDias, etCantidadLlegar;
+    EditText etCantidad, etDias, etCantidadLlegar, etCantidadInicial;
     Button btnCalcular, btnCrearCalc;
     String dateResultado;
+    Switch swInicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +34,12 @@ public class CalculadoraActivity extends AppCompatActivity implements View.OnCli
         etCantidadLlegar = findViewById(R.id.etCantidadLlegar);
         btnCalcular = findViewById(R.id.btnCalcular);
         btnCrearCalc = findViewById(R.id.btnCrearCalc);
+        swInicio = findViewById(R.id.swInicioCalc);
+        etCantidadInicial = findViewById(R.id.etCantidadInicialCalc);
 
         btnCalcular.setOnClickListener(this);
         btnCrearCalc.setOnClickListener(this);
+        swInicio.setOnClickListener(this);
 
     }
 
@@ -42,41 +47,41 @@ public class CalculadoraActivity extends AppCompatActivity implements View.OnCli
     public void onClick(View v) {
         if (v.getId() == R.id.btnCalcular) {
             // Calcular el resultado
-            calcular();
+            comporbar();
         } else if (v.getId() == btnCrearCalc.getId()) {
             // Crear el objetivo con los datos
             mandarObjetivo();
+        } else if (v.getId() == swInicio.getId()) {
+            // Mostrar el campo de cantidad inicial
+            if (swInicio.isChecked()) {
+                etCantidadInicial.setVisibility(View.VISIBLE);
+            } else {
+                etCantidadInicial.setVisibility(View.GONE);
+            }
         }
     }
 
-    private void calcular() {
+    private void comporbar() {
         if (leerNumero(etCantidad.getText().toString(), etCantidad)) {
             if (leerNumero(etDias.getText().toString(), etDias)) {
                 if (leerNumero(etCantidadLlegar.getText().toString(), etCantidadLlegar)) {
-                    float cantidad = Float.parseFloat(etCantidad.getText().toString());
-                    int dias = Integer.parseInt(etDias.getText().toString());
-                    float cantidadLlegar = Float.parseFloat(etCantidadLlegar.getText().toString());
-                    if (cantidadLlegar < cantidad) {
-                        etCantidadLlegar.setError(getString(R.string.cantidad_menor));
-                    } else {
-                        float resultado = cantidadLlegar / cantidad;
-                        BigDecimal bd = new BigDecimal(resultado);
-                        bd = bd.setScale(0, BigDecimal.ROUND_CEILING);
-                        int porCanto = bd.intValue();
-                        int diasSuma = porCanto * dias;
-
-                        LocalDate localDate;
-
-                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                            localDate = LocalDate.now();
-                            localDate = localDate.plusDays(diasSuma);
-                            DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-                            dateResultado = localDate.format(formato);
-                            tvResultado.setText(getString(R.string.calculadora_text) + dateResultado);
-                            tvResultado.setVisibility(View.VISIBLE);
-                            tvTextCrearCalc.setVisibility(View.VISIBLE);
-                            btnCrearCalc.setVisibility(View.VISIBLE);
+                    if (swInicio.isChecked()) {
+                        if (leerNumero(etCantidadInicial.getText().toString(), etCantidadInicial)) {
+                            float cantidad = Float.parseFloat(etCantidad.getText().toString());
+                            int dias = Integer.parseInt(etDias.getText().toString());
+                            float cantidadLlegar = Float.parseFloat(etCantidadLlegar.getText().toString());
+                            float cantidadInicial = Float.parseFloat(etCantidadInicial.getText().toString());
+                            calcular(cantidad, dias, cantidadLlegar, cantidadInicial);
+                        } else {
+                            tvResultado.setVisibility(View.GONE);
+                            tvTextCrearCalc.setVisibility(View.GONE);
+                            btnCrearCalc.setVisibility(View.GONE);
                         }
+                    } else {
+                        float cantidad = Float.parseFloat(etCantidad.getText().toString());
+                        int dias = Integer.parseInt(etDias.getText().toString());
+                        float cantidadLlegar = Float.parseFloat(etCantidadLlegar.getText().toString());
+                        calcular(cantidad, dias, cantidadLlegar, 0);
                     }
                 } else {
                     tvResultado.setVisibility(View.GONE);
@@ -92,6 +97,61 @@ public class CalculadoraActivity extends AppCompatActivity implements View.OnCli
             tvResultado.setVisibility(View.GONE);
             tvTextCrearCalc.setVisibility(View.GONE);
             btnCrearCalc.setVisibility(View.GONE);
+        }
+    }
+
+    private void calcular(float cantidad, int dias, float cantidadLlegar, float cantidadInicial) {
+        if (cantidadInicial == 0) {
+            if (cantidadLlegar < cantidad) {
+                etCantidadLlegar.setError(getString(R.string.cantidad_menor));
+            } else {
+                float resultado = cantidadLlegar / cantidad;
+                BigDecimal bd = new BigDecimal(resultado);
+                bd = bd.setScale(0, BigDecimal.ROUND_CEILING);
+                int porCanto = bd.intValue();
+                int diasSuma = porCanto * dias;
+
+                LocalDate localDate;
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    localDate = LocalDate.now();
+                    localDate = localDate.plusDays(diasSuma);
+                    DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                    dateResultado = localDate.format(formato);
+                    tvResultado.setText(getString(R.string.calculadora_text) + dateResultado);
+                    tvResultado.setVisibility(View.VISIBLE);
+                    tvTextCrearCalc.setVisibility(View.VISIBLE);
+                    btnCrearCalc.setVisibility(View.VISIBLE);
+                }
+            }
+        } else {
+            if (cantidadInicial > cantidadLlegar) {
+                etCantidadInicial.setError(getString(R.string.cantidad_menor));
+            } else {
+                cantidadLlegar = cantidadLlegar - cantidadInicial;
+                if (cantidadLlegar < cantidad) {
+                    etCantidadLlegar.setError(getString(R.string.cantidad_menor));
+                } else {
+                    float resultado = cantidadLlegar / cantidad;
+                    BigDecimal bd = new BigDecimal(resultado);
+                    bd = bd.setScale(0, BigDecimal.ROUND_CEILING);
+                    int porCanto = bd.intValue();
+                    int diasSuma = porCanto * dias;
+
+                    LocalDate localDate;
+
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                        localDate = LocalDate.now();
+                        localDate = localDate.plusDays(diasSuma);
+                        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                        dateResultado = localDate.format(formato);
+                        tvResultado.setText(getString(R.string.calculadora_text) + dateResultado);
+                        tvResultado.setVisibility(View.VISIBLE);
+                        tvTextCrearCalc.setVisibility(View.VISIBLE);
+                        btnCrearCalc.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
         }
     }
 
@@ -114,10 +174,21 @@ public class CalculadoraActivity extends AppCompatActivity implements View.OnCli
     }
 
     private void mandarObjetivo() {
-        Intent intent = new Intent(this, AddObjetivosActivity.class);
-        intent.putExtra("rDatos", true);
-        intent.putExtra("fecha", dateResultado);
-        intent.putExtra("cantidad", etCantidadLlegar.getText().toString());
-        startActivity(intent);
+        if (swInicio.isChecked()) {
+            float cantidadInicial = Float.parseFloat(etCantidadInicial.getText().toString());
+            float cantidadLlegar = Float.parseFloat(etCantidadLlegar.getText().toString());
+            float cantidad = cantidadLlegar- cantidadInicial;
+            Intent intent = new Intent(this, AddObjetivosActivity.class);
+            intent.putExtra("rDatos", true);
+            intent.putExtra("fecha", dateResultado);
+            intent.putExtra("cantidad", cantidad+"");
+            startActivity(intent);
+        } else{
+            Intent intent = new Intent(this, AddObjetivosActivity.class);
+            intent.putExtra("rDatos", true);
+            intent.putExtra("fecha", dateResultado);
+            intent.putExtra("cantidad", etCantidadLlegar.getText().toString());
+            startActivity(intent);
+        }
     }
 }
