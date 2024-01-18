6a7a2ee661642b9e2476df918bfe1940ebeb5da7
Aritz-Garcia <73273260+Aritz-Garcia@users.noreply.github.com>
2024-01-18 16:57:52 +0100

    Ahorro en tiempo general

--- SaveGoals/app/src/main/java/com/savegoals/savegoals/controlador/estObjetivos/EstEvolucionFragment.java
+++ SaveGoals/app/src/main/java/com/savegoals/savegoals/controlador/estObjetivos/EstEvolucionFragment.java
@@ -1,5 +1,6 @@
 package com.savegoals.savegoals.controlador.estObjetivos;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +15,18 @@ import com.savegoals.savegoals.data.entities.Objetivos;
 import com.savegoals.savegoals.db.AppDatabase;
 
 import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
 
 public class EstEvolucionFragment extends Fragment {
 
-    TextView tvPorcentaje, tvAhorrado, tvPendiente, tvTotal, tvAhorradotxt, tvPendientetxt, tvTotaltxt, tvError;
+    TextView tvPorcentaje, tvAhorrado, tvPendiente, tvTotal, tvAhorradotxt, tvPendientetxt, tvTotaltxt, tvError, tvText,
+            tvDiatxt, tvSemanatxt, tvMestxt, tvDia, tvSemana, tvMes;
     ProgressBar progressBar;
 
     public EstEvolucionFragment() {
@@ -44,6 +52,13 @@ public class EstEvolucionFragment extends Fragment {
         tvPendientetxt = view.findViewById(R.id.tvPendientetxtEst);
         tvTotaltxt = view.findViewById(R.id.tvTotaltxtEst);
         tvError = view.findViewById(R.id.tvErrorEstEvolucion);
+        tvText = view.findViewById(R.id.tvTextObjTotal);
+        tvDiatxt = view.findViewById(R.id.tvDiatxtObjTotal);
+        tvSemanatxt = view.findViewById(R.id.tvSemanatxtObjTotal);
+        tvMestxt = view.findViewById(R.id.tvMestxtObjTotal);
+        tvDia = view.findViewById(R.id.tvDiaObjTotal);
+        tvSemana = view.findViewById(R.id.tvSemanaObjTotal);
+        tvMes = view.findViewById(R.id.tvMesObjTotal);
 
         return view;
     }
@@ -75,6 +90,13 @@ public class EstEvolucionFragment extends Fragment {
             tvAhorradotxt.setVisibility(View.VISIBLE);
             tvPendientetxt.setVisibility(View.VISIBLE);
             tvTotaltxt.setVisibility(View.VISIBLE);
+            tvText.setVisibility(View.VISIBLE);
+            tvDiatxt.setVisibility(View.VISIBLE);
+            tvSemanatxt.setVisibility(View.VISIBLE);
+            tvMestxt.setVisibility(View.VISIBLE);
+            tvDia.setVisibility(View.VISIBLE);
+            tvSemana.setVisibility(View.VISIBLE);
+            tvMes.setVisibility(View.VISIBLE);
 
             for (int i = 0; i < objetivos.size(); i++) {
                 total = total + objetivos.get(i).getCantidad();
@@ -112,6 +134,41 @@ public class EstEvolucionFragment extends Fragment {
             tvPendiente.setText(obtieneDosDecimales(pendiente) + "€");
             tvTotal.setText(obtieneDosDecimales(total) + "€");
 
+            List<Objetivos> objetivosSinTerminar = appDatabase.objetivosDao().getAllNotCompleted();
+            float dia = 0;
+            float semana = 0;
+            float mes = 0;
+
+            SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+            Date today = new Date();
+            today.setHours(0);
+            today.setMinutes(0);
+            today.setSeconds(0);
+            Date fechaFin = null;
+            for (int i = 0; i < objetivosSinTerminar.size(); i++) {
+                try {
+                    fechaFin = formato.parse(objetivosSinTerminar.get(i).getFecha());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                float pendienteSinTerminar = objetivosSinTerminar.get(i).getCantidad() - objetivosSinTerminar.get(i).getAhorrado();
+
+                if (leerFecha(today, fechaFin)) {
+                    int dias = getDias(today, fechaFin);
+                    int semanas = getSemanas(today, fechaFin);
+                    int meses = getMeses(today, fechaFin);
+
+                    dia = dia + pendienteSinTerminar / dias;
+                    semana = semana + pendienteSinTerminar / semanas;
+                    mes = mes + pendienteSinTerminar / meses;
+
+                    tvDia.setText(obtieneDosDecimales(dia) + "€");
+                    tvSemana.setText(obtieneDosDecimales(semana) + "€");
+                    tvMes.setText(obtieneDosDecimales(mes) + "€");
+                }
+            }
+
+
         } else {
             tvPorcentaje.setVisibility(View.GONE);
             tvAhorrado.setVisibility(View.GONE);
@@ -121,6 +178,13 @@ public class EstEvolucionFragment extends Fragment {
             tvAhorradotxt.setVisibility(View.GONE);
             tvPendientetxt.setVisibility(View.GONE);
             tvTotaltxt.setVisibility(View.GONE);
+            tvText.setVisibility(View.GONE);
+            tvDiatxt.setVisibility(View.GONE);
+            tvSemanatxt.setVisibility(View.GONE);
+            tvMestxt.setVisibility(View.GONE);
+            tvDia.setVisibility(View.GONE);
+            tvSemana.setVisibility(View.GONE);
+            tvMes.setVisibility(View.GONE);
             tvError.setVisibility(View.VISIBLE);
         }
 
@@ -129,6 +193,52 @@ public class EstEvolucionFragment extends Fragment {
     private static String obtieneDosDecimales(float value){
         DecimalFormat df = new DecimalFormat("0.00");
         return df.format(value);
+    }
+
+    private int getDias(Date hoy, Date fechaFin) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LocalDate hoyLocal = hoy.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            LocalDate fechaFinLocal = fechaFin.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            return (int) ChronoUnit.DAYS.between(hoyLocal, fechaFinLocal);
+        }
+        return 1;
+    }
+
+    private int getSemanas(Date hoy, Date fechaFin) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LocalDate hoyLocal = hoy.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            LocalDate fechaFinLocal = fechaFin.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+            long dias = ChronoUnit.DAYS.between(hoyLocal, fechaFinLocal);
+            if (dias > 7) {
+                return (int) (dias / 7);
+            }
+        }
+        return 1;
+    }
+
+    private int getMeses(Date hoy, Date fechaFin) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LocalDate hoyLocal = hoy.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            LocalDate fechaFinLocal = fechaFin.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+            long dias = ChronoUnit.DAYS.between(hoyLocal, fechaFinLocal);
+            if (dias > 30) {
+                return (int) (dias / 30);
+            }
+        }
+        return 1;
+    }
+
+    private boolean leerFecha(Date today, Date fechaFin){
+
+        if (fechaFin.getDay() == today.getDay() && fechaFin.getMonth() == today.getMonth() && fechaFin.getYear() == today.getYear()) {
+            return true;
+        } else if (fechaFin.before(today)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
 }
